c34368337b8f213319f350b983ec79c6a0ceb13b
Robin Brouwer <robin@45north.nl>
2011-08-30 10:10:42 +0200

    Seperated install_locales.

--- lib/gritter.rb
+++ lib/gritter.rb
@@ -1,6 +1,7 @@
 require 'gritter/helpers'
 require 'gritter/gflash'
 require 'gritter/engine'
+require 'fileutils'
 
 module Gritter
   def self.initialize
@@ -8,20 +9,27 @@ module Gritter
     raise "ActionController is not available yet." unless defined?(ActionController)
     ActionController::Base.send(:helper, Gritter::Helpers)
     ActionController::Base.send(:include, Gritter::Gflash)
+    Gritter.install_locales
     Gritter.install_gritter if ::Rails.version < "3.1"
     @initialized = true
   end
   
+  def self.install_locales
+    orig_lang = File.join(File.dirname(__FILE__), 'gritter', 'assets', 'gflash.en.yml')
+    dest_lang = File.join(Rails.root, 'config', 'locales', 'gflash.en.yml')
+    unless File.exists?(dest_lang)
+      puts "Copying language file to #{dest_lang}..."
+      FileUtils.cp_r orig_lang, dest_lang
+    end
+  end
+  
   def self.install_gritter
-    require 'fileutils'
     orig_javascripts = File.join(File.dirname(__FILE__), 'gritter', 'assets', 'javascripts')
     orig_stylesheets = File.join(File.dirname(__FILE__), 'gritter', 'assets', 'stylesheets')
     orig_images = File.join(File.dirname(__FILE__), 'gritter', 'assets', 'images')
-    orig_lang = File.join(File.dirname(__FILE__), 'gritter', 'assets', 'gflash.en.yml')
     dest_javascripts = File.join(Rails.root, 'public', 'javascripts', 'gritter')
     dest_stylesheets = File.join(Rails.root, 'public', 'stylesheets', 'gritter')
     dest_images = File.join(Rails.root, 'public', 'images', 'gritter')
-    dest_lang = File.join(Rails.root, 'config', 'locales', 'gflash.en.yml')
     
     gritter = File.join(dest_javascripts, 'jquery.gritter.min.js')
 
@@ -39,8 +47,6 @@ module Gritter
         FileUtils.cp_r "#{orig_stylesheets}/.", dest_stylesheets
         puts "Copying gritter to #{dest_images}..."
         FileUtils.cp_r "#{orig_images}/.", dest_images
-        puts "Copying language file to #{dest_lang}..."
-        FileUtils.cp_r orig_lang, dest_lang
         puts "Successfully installed gritter."
       rescue
         puts "ERROR: Problem installing gritter. Please copy the files manually."
