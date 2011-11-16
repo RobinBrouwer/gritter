95c86d06f575d37a4f51c41913b170a1b01d48c4
Robin Brouwer <robin@45north.nl>
2011-11-16 15:19:56 +0100

    Removed old gritter file installer.

--- lib/gritter.rb
+++ lib/gritter.rb
@@ -10,7 +10,6 @@ module Gritter
     ActionController::Base.send(:helper, Gritter::Helpers)
     ActionController::Base.send(:include, Gritter::Gflash)
     Gritter.install_locales
-    Gritter.install_gritter if ::Rails.version < "3.1"
     @initialized = true
   end
   
@@ -20,37 +19,6 @@ module Gritter
     unless File.exists?(dest_lang)
       puts "Copying language file to #{dest_lang}..."
       FileUtils.cp_r orig_lang, dest_lang
-    end
-  end
-  
-  def self.install_gritter
-    orig_javascripts = File.join(File.dirname(__FILE__), 'gritter', 'assets', 'javascripts')
-    orig_stylesheets = File.join(File.dirname(__FILE__), 'gritter', 'assets', 'stylesheets')
-    orig_images = File.join(File.dirname(__FILE__), 'gritter', 'assets', 'images')
-    dest_javascripts = File.join(Rails.root, 'public', 'javascripts', 'gritter')
-    dest_stylesheets = File.join(Rails.root, 'public', 'stylesheets', 'gritter')
-    dest_images = File.join(Rails.root, 'public', 'images', 'gritter')
-    
-    gritter = File.join(dest_javascripts, 'jquery.gritter.min.js')
-
-    unless File.exists?(gritter) && FileUtils.identical?(File.join(orig_javascripts, 'jquery.gritter.min.js'), gritter)
-      begin
-        puts "Creating directory #{dest_javascripts}..."
-        FileUtils.mkdir_p dest_javascripts
-        puts "Creating directory #{dest_stylesheets}..."
-        FileUtils.mkdir_p dest_stylesheets
-        puts "Creating directory #{dest_images}..."
-        FileUtils.mkdir_p dest_images
-        puts "Copying gritter to #{dest_javascripts}..."
-        FileUtils.cp_r "#{orig_javascripts}/.", dest_javascripts
-        puts "Copying gritter to #{dest_stylesheets}..."
-        FileUtils.cp_r "#{orig_stylesheets}/.", dest_stylesheets
-        puts "Copying gritter to #{dest_images}..."
-        FileUtils.cp_r "#{orig_images}/.", dest_images
-        puts "Successfully installed gritter."
-      rescue
-        puts "ERROR: Problem installing gritter. Please copy the files manually."
-      end
     end
   end
 end
