11bcca59deaada7a05024d264331b2766d763708
Robin Brouwer <robin@45north.nl>
2011-11-16 15:50:50 +0100

    No need for rake file.

--- lib/gritter/railtie.rb
+++ lib/gritter/railtie.rb
@@ -1,10 +1,6 @@
 if defined?(Rails::Railtie)
   module Gritter
     class Railtie < Rails::Railtie
-      rake_tasks do
-        load "tasks/gritter.rake"
-      end
-
       initializer :gritter do
         Gritter.initialize
       end
--- lib/tasks/gritter.rake
+++
@@ -1,6 +0,0 @@
-namespace :gritter do
-  desc 'Install the gritter sources'
-  task :install => :environment do
-    Gritter.install_gritter
-  end
-end
