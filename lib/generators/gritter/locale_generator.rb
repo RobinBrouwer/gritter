321f9f822ce3c24d77b9a4482ffedb935240e7b6
Robin Brouwer <robin@45north.nl>
2011-11-16 16:05:10 +0100

    Oops. Wrong file name.

--- lib/generators/gritter/locale_generator.rb
+++ lib/generators/gritter/locale_generator.rb
@@ -2,8 +2,8 @@ module Gritter
   class LocaleGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
     
-    def copy_gritter_locale
-      copy_file "gritter.en.yml", "config/locales/gritter.en.yml"
+    def copy_gflash_locale
+      copy_file "gflash.en.yml", "config/locales/gflash.en.yml"
     end
   end
 end
