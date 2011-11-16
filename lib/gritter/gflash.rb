7ef1d4a9cc611d5632f2a2ba63c06e8d11ab57c3
Robin Brouwer <robin@45north.nl>
2011-11-16 20:58:46 +0100

    Refactored gflash methods. Adding locale gflash messages is now easier than ever! Just do `gflash :success, :error, :notice` instead of `gflash :success => true, :error => true, :notice => true`. The old syntax is still supported.

--- lib/gritter/gflash.rb
+++ lib/gritter/gflash.rb
@@ -3,26 +3,36 @@ module Gritter
     def gflash *args
       session[:gflash] ||= {}
       options = args.extract_options!
+      
+      if args.present?
+        args.each do |key|
+          gflash_push(key, gflash_translation(key))
+        end
+      end
+      
       options.each do |key, value|
         if value.is_a?(Hash)
-          gflash_value = value
-          gflash_value[:value] = gflash_text(key, gflash_value[:value]) if gflash_value.has_key?(:value)
+          value[:value] = gflash_text(key, value[:value]) if value.has_key?(:value)
         else
-          gflash_value = gflash_text(key, value)
+          value = gflash_text(key, value)
         end
-        
-        if session[:gflash].has_key?(key)
-          session[:gflash][key].push(gflash_value)
-        else
-          session[:gflash][key] = [gflash_value]
-        end
+        gflash_push(key, value)
       end
     end
   
   private
     
     def gflash_text(key, value)
-      value == true ? I18n.t("gflash.#{params[:controller]}.#{params[:action]}.#{key}") : value
+      value == true ? gflash_translation(key) : value
+    end
+    
+    def gflash_push(key, value)
+      session[:gflash][key] ||= []
+      session[:gflash][key].push(value)
+    end
+    
+    def gflash_translation(key)
+      I18n.t("gflash.#{params[:controller]}.#{params[:action]}.#{key}")
     end
   end
 end
